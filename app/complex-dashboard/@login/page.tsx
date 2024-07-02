4d5202627a235235e4c5d1878816d013b43cdc51
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-02 16:47:28 +0200

    conditonal routes

---
+++ app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,12 @@
+import Card from '@/components/card'
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+       <Card>Please login to continue...</Card>
+    </>
+  )
+}
+
+export default Login
--- app/complex-dashboard/layout.tsx
+++ app/complex-dashboard/layout.tsx
@@ -1,28 +1,30 @@
-
-
-function layout({children, users, revenue, notifications}: {
-    children: React.ReactNode;
-    users: React.ReactNode;
-    revenue: React.ReactNode;
-    notifications: React.ReactNode;
+function DashboardLayout({
+  children,
+  users,
+  revenue,
+  notifications,
+  login
+}: {
+  children: React.ReactNode;
+  users: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = false;
+  return isLoggedIn ? (
     <div className="flex flex-col">
-        {/* Content from page.tsx */}
-        <div>{children}</div>
-        <div className="flex gap-3 flex-wrap flex-1">
-            <div className="flex flex-col gap-3 flex-1">
-                <div>{users}</div>
-                <div>{revenue}</div>
-            </div>
-            <div className="flex flex-1">
-                {notifications}
-            </div>
+      {/* Content from page.tsx */}
+      <div>{children}</div>
+      <div className="flex gap-3 flex-wrap flex-1">
+        <div className="flex flex-col gap-3 flex-1">
+          <div>{users}</div>
+          <div>{revenue}</div>
         </div>
-
-        
+        <div className="flex flex-1">{notifications}</div>
+      </div>
     </div>
-  )
+  ) : login
 }
 
-export default layout
+export default DashboardLayout;
